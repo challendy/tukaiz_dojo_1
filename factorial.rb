927fbd639cb25bae81999ccf7980651e5812d02b
Chris Hallendy <chris.hallendy@gmail.com>
2011-04-04 11:49:31 -0400

    fixing branch issue

--- factorial.rb
+++ factorial.rb
@@ -1,24 +1,8 @@
 class Factorial
     
   # Sample Factorial method naming
-  def factorial_6918(n)
-    result = n
-    if n > 0
-      while n > 1 do
-        result = result * (n-1)
-        n = n-1        
-      end
-    elsif n == 0
-      result = 1    
-    else
-      result = result.to_s
-      result += " is not a valid integer"
-    end   
-    result
+  def factorial_1234(n)
   end
 
 end
 
-
-app = Factorial.new
-puts app.factorial_6918(10)
