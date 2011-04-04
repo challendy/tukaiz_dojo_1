f75038b3e354871b8ed75947602b9654fbaa3c4e
George Diaz <g.diaz@tukaiz.com>
2011-04-04 14:12:17 -0500

    George's fatorial code

--- factorial.rb
+++ factorial.rb
@@ -1,8 +1,13 @@
 class Factorial
     
   # Sample Factorial method naming
-  def factorial_1234(n)
+  def factorial_6912(n)
+    case n
+    when 0..1
+      n
+    else
+      n * factorial_6912(n-1)
+    end
   end
-
 end
 
