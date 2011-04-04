60638435a6254cec731157fdd588ff66ea9fac73
Tukaiz <tukaiz@studioimac13.tukaiz.com>
2011-04-04 12:21:19 -0500

    Tukaiz Dojo - Factorial

--- factorial.rb
+++ factorial.rb
@@ -1,8 +1,16 @@
 class Factorial
     
-  # Sample Factorial method naming
-  def factorial_1234(n)
+  # Factorial of 0 = 1 
+  def factorial_4924(n=nil)
+    if n.kind_of?(Fixnum) and n >= 0
+      return 1 if n == 0
+      r=1
+      (1..n).each{ |n| r = r*n }
+      r
+    else
+      raise ArgumentError, "Wrong argument - Positive Integer Required"
+    end   
   end
-
+  
 end
 
