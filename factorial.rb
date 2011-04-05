c5f514860e5bcc0bb64bd1ee6cd10e6ea4d80044
christukaiz <c.gordon@tukaiz.com>
2011-04-05 12:52:13 -0500

    Chris G's Factorial Fun

--- factorial.rb
+++ factorial.rb
@@ -1,8 +1,22 @@
 class Factorial
-    
-  # Sample Factorial method naming
-  def factorial_1234(n)
+
+  # Most readable to non-ruby programmer
+  def factorial_4923(n)
+    if n <= 1
+      1
+    else
+      n * factorial_4923(n - 1)
+    end
+  end  
+
+  # Most consise - Ruby 1.8.7+ required (wont work on w2p or webapps servers)
+  def factorial_4923_consise(n)
+    (1..n).reduce(1, :*)
+  end
+
+  # Most readable to Courtney "The Injector" Braafhart
+  def factorial_4923_cb(n)
+    (1..n).inject(1) {|total, n| total * n }
   end
 
 end
-
