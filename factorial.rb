1c6f6f308841b60f9bbe9d8b175587ed2a363986
Carolina Soler-Rodriguez <carolinas998@gmail.com>
2011-04-05 13:44:23 -0500

    Added my factorial

--- factorial.rb
+++ factorial.rb
@@ -1,8 +1,29 @@
 class Factorial
+  #C:\Users\Carolina\.ssh\tukaiz_dojo_1\factorial.rb
     
   # Sample Factorial method naming
-  def factorial_1234(n)
+  def factorial_4820(n)
+	
+  	case 
+  	  when n < 0
+  		"undefined" 
+	  when n == 0
+		0
+	  else
+		i, val = 1, 1
+		while i <= n
+	  	  val = val * i
+		  i += 1
+		end
+		val
+	end
   end
-
+  
 end
 
+fact = Factorial.new
+puts "Factorial of -1:  #{fact.factorial_casoler(-1)}"
+puts "Factorial of 0:  #{fact.factorial_casoler(0)}"
+puts "Factorial of 1:  #{fact.factorial_casoler(1)}"
+puts "Factorial of 3:  #{fact.factorial_casoler(3)}"
+puts "Factorial of 10:  #{fact.factorial_casoler(10)}"
