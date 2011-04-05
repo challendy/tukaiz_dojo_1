2f31fad310aa363dc8445f0bacb10a1f17cb2b5e
Nick Lewis <nlewisinc@gmail.com>
2011-04-05 09:22:18 -0500

    factorial_4819

--- factorial.rb
+++ factorial.rb
@@ -1,8 +1,16 @@
 class Factorial
-    
+
   # Sample Factorial method naming
   def factorial_1234(n)
   end
 
+  # => making this a class method so its easier to call
+  def self.factorial_4819(n)
+    n == 0 ? 1 : n * factorial_4819(n - 1)
+  end
 end
+
+# command line: ruby factorial.rb
+puts Factorial.factorial_4819(5)
+# => 120
 
