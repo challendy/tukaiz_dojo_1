7d3c6e219b02f4523ca3c4bbd3605405e75ac18b
Sara Trice <tukaiz@studioimac11.tukaiz.com>
2011-04-08 11:39:20 -0500

    Here's my shot at it

--- factorial.rb
+++ factorial.rb
@@ -1,7 +1,19 @@
 class Factorial
     
   # Sample Factorial method naming
-  def factorial_1234(n)
+  def factorial_4962(n)
+    # my not very human readable solution
+    # x=1
+    # (1..n).step(1).map{|y| x = x * y}.last
+
+    # my other solution
+    range = 1..n
+    array = []
+    range.each do |number|
+      array << number
+    end
+    array.reverse!
+    eval array.join(" * ")
   end
 
 end
